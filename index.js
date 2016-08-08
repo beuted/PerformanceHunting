92985ec98d9a288a78b98dfef765a20f59eb8875
dekajoo <dekajoo@gmail.com>
2016-08-08 20:56:12 +0200

    Allowing user to login and set a position
    Big commit sry about that

--- index.js
+++ index.js
@@ -1,68 +1,90 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var PokemonGO = require('pokemon-go-node-api')
 var _ = require('lodash');
 
 var app = express();
 var port = process.env.PORT || 3000;
-var pokeio = new PokemonGO.Pokeio();
 var config = require('./config');
 
 // Serve static files
 app.use(express.static('public'));
+// To support JSON-encoded bodies
+app.use(bodyParser.json());
 
 
 var server = app.listen(port, function() {
-    console.log('csgo-mates-server is running at localhost:' + port);
+    console.log('performance-hunting is running at localhost:' + port);
 });
 
 var io = require('socket.io').listen(server);
 
+var provider = process.env.PGO_PROVIDER || config.PGO_PROVIDER;
+
 // Routing
-app.get('/api/pokemons', (req, res) => {
-    res.status(200).send('tones of them');
-    res.end();
+app.post('/api/login', (req, res) => {
+    var json = req.body;
+    var locationObj = { type: 'name', name: json.location };
+
+    console.log(JSON.stringify(locationObj));
+
+    watchPokemonsInZone(json.user, json.password, locationObj, provider, function(successMsg, errorMsg) {
+        if (!errorMsg) {
+            res.status(200).send({ 'message': successMsg });
+        } else {
+            res.status(401).send({ 'message': errorMsg });
+        }
+        res.end();
+    });
+});
+
+// Web Sockets
+var socket;
+io.sockets.on('connection', (s) => {
+    socket = s;
+    console.log(`[s] client connected: ${socket.id}`);
 });
 
 // Pokemon api logic
-var location = {
-    type: 'name',
-    name: process.env.PGO_LOCATION || '48.87929, 2.33203'
-};
+var watchPokemonsInZone = function(username, password, location, provider, callback) {
+var pokeio = new PokemonGO.Pokeio();
+    pokeio.init(username, password, location, provider, function(err) {
+        if (err) {
+            var errorMsg = `[Error] Unable to connect with: (${username}, ${password}, ${provider}) at "${location.name}"`;
+            console.error(`${errorMsg}\n-> err: ${JSON.stringify(err)}`);
+            callback(null, errorMsg);
+            return;
+        }
+        callback(`Successfully logged with: (${username}, ${password}, ${provider}) at ${location.name}`, null);
 
-var username = process.env.PGO_USERNAME || config.PGO_USERNAME;
-var password = process.env.PGO_PASSWORD || config.PGO_PASSWORD;
-var provider = process.env.PGO_PROVIDER || config.PGO_PROVIDER;
+        console.log('[i] Current location: ' + pokeio.playerInfo.locationName);
+        console.log('[i] lat/long/alt: : ' + pokeio.playerInfo.latitude + ' ' + pokeio.playerInfo.longitude + ' ' + pokeio.playerInfo.altitude);
 
-pokeio.init(username, password, location, provider, function(err) {
-    if (err) throw err;
+    // Note(b.jehanno) Code commented cause of throttling issue (connection + first call to profile are too close to eachother)
+    //    pokeio.GetProfile(function(err, profile) {
+    //        if (err) throw err;
+    //
+    //        console.log('[i] Username: ' + profile.username);
+    //        console.log('[i] Poke Storage: ' + profile.poke_storage);
+    //        console.log('[i] Item Storage: ' + profile.item_storage);
+    //
+    //        var poke = 0;
+    //        if (profile.currency[0].amount) {
+    //            poke = profile.currency[0].amount;
+    //        }
+    //
+    //        console.log('[i] Pokecoin: ' + poke);
+    //        console.log('[i] Stardust: ' + profile.currency[1].amount);
+    //    });
 
-    console.log('[i] Current location: ' + pokeio.playerInfo.locationName);
-    console.log('[i] lat/long/alt: : ' + pokeio.playerInfo.latitude + ' ' + pokeio.playerInfo.longitude + ' ' + pokeio.playerInfo.altitude);
-
-// Note(b.jehanno) Code commented cause of throttling issue (connection + first call to profile are too close to eachother)
-//    pokeio.GetProfile(function(err, profile) {
-//        if (err) throw err;
-//
-//        console.log('[i] Username: ' + profile.username);
-//        console.log('[i] Poke Storage: ' + profile.poke_storage);
-//        console.log('[i] Item Storage: ' + profile.item_storage);
-//
-//        var poke = 0;
-//        if (profile.currency[0].amount) {
-//            poke = profile.currency[0].amount;
-//        }
-//
-//        console.log('[i] Pokecoin: ' + poke);
-//        console.log('[i] Stardust: ' + profile.currency[1].amount);
-//    });
-
-    var allPokemonsSeen = []; //TODO(b.jehanno): Store this in a fancy DB (elastic search ?)
-    io.sockets.on('connection', (socket) => {
-        console.log('[s] client connected');
+        var allPokemonsSeen = []; //TODO(b.jehanno): Store this in a fancy DB (elastic search ?)
         setInterval(() => {
             pokeio.Heartbeat(function(err, hb) {
-                if(err)
-                    console.error(err);
+                console.log('toudoum: ' + username); //TODO: remove
+                if(err || hb === undefined) {
+                    console.error(`[Error] Search for pokemon failed\n-> err: ${JSON.stringify(err)}`);
+                    return;
+                }
 
                 var pokemonsSeen = [];
                 for (var i = hb.cells.length - 1; i >= 0; i--) {
@@ -93,4 +115,5 @@ pokeio.init(username, password, location, provider, function(err) {
             });
         }, 10000);
     });
-});
+}
+
