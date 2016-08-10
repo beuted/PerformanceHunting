cc792f2ba2b0d410502256117fea1c717ddf0fe1
dekajoo <dekajoo@gmail.com>
2016-08-11 01:09:50 +0200

    multiply radius scanned by two

--- index.js
+++ index.js
@@ -55,7 +55,9 @@ app.post('/api/move', (req, res) => {
             res.status(500).send({ 'message': errorMsg });
             res.end();
         } else {
-            console.log(`[i] User ${json.username} moved to ${JSON.stringify(json.location)}`);
+            user.lookingPointIndex = 0;
+            user.location = json.location;
+            console.log(`[i] User ${json.username} moved to ${JSON.stringify(locationObj.coords)}`);
             res.status(200).send(msg);
             res.end();
         }
@@ -102,40 +104,90 @@ var watchPokemonsInZone = function(username, password, location, provider, socke
 
         callback({ position: { lat: pokeio.playerInfo.latitude, lng: pokeio.playerInfo.longitude } }, null);
 
-        var heartbeatId = setInterval(() => {
-            pokeio.Heartbeat(function(err, hb) {
-                console.log('[HB] - ' + username); //TODO: remove
-                if(err || hb === undefined) {
-                    console.error(`[error] Search for pokemon failed\n-> err: ${JSON.stringify(err)}`);
-                    return;
-                }
+        var lookingPointsIndex = 0;
 
-                //TODO(b.jehanno): Store this in a fancy DB (elastic search ?)
-                var pokemonsSeen = [];
-                for (var i = hb.cells.length - 1; i >= 0; i--) {
-                    for (var j = hb.cells[i].MapPokemon.length - 1; j >= 0; j--)
-                    {
-                        var currentPokemon = hb.cells[i].MapPokemon[j];
-                        var pokedexInfo = pokeio.pokemonlist[parseInt(currentPokemon.PokedexTypeId)-1]; //TODO(b.jehanno): let's store in client
-                        var pokemon = {
-                            encounterId: currentPokemon.EncounterId.toNumber(),
-                            name: pokedexInfo.name,
-                            lat: currentPokemon.Latitude,
-                            lng: currentPokemon.Longitude,
-                            expirationTimeMs: currentPokemon.ExpirationTimeMs.toNumber(),
-                            image: pokedexInfo.img
-                        }
-                        pokemonsSeen.push(pokemon);
-                        console.log('[+] ' + pokedexInfo.name + ' found: ' + JSON.stringify(pokemon));
-                    }
+        var heartbeatId = setInterval(() => {
+            var lookingPoints = getSurroundingPoints(loggedUsers[username].location);
+
+            var locationObj = {
+                type: 'coords',
+                coords: {
+                    latitude: lookingPoints[loggedUsers[username].lookingPointIndex].lat,
+                    longitude: lookingPoints[loggedUsers[username].lookingPointIndex].lng
                 }
-                if (pokemonsSeen.length > 0)
-                    io.sockets.emit('new_pokemons', pokemonsSeen);
+            };
+
+            pokeio.SetLocation(locationObj, (err, msg) => {
+                if (err) {
+                    var errorMsg = `[error] Unable to move to: ${JSON.stringify(locationObj.coords)} as ${username}"`;
+                    console.error(`${errorMsg}\n-> err: ${JSON.stringify(err)}`);
+                } else {
+                    pokeio.Heartbeat(function(err, hb) {
+                        console.log(`[HB] - ${username} Scanning ${JSON.stringify(locationObj.coords)}`);
+                        if (err || hb === undefined) {
+                            console.error(`[error] Search for pokemon failed\n-> err: ${JSON.stringify(err)}`);
+                            return;
+                        }
+
+                        //TODO(b.jehanno): Store this in a fancy DB (elastic search ?)
+                        var pokemonsSeen = [];
+                        for (var i = hb.cells.length - 1; i >= 0; i--) {
+                            for (var j = hb.cells[i].MapPokemon.length - 1; j >= 0; j--)
+                            {
+                                var currentPokemon = hb.cells[i].MapPokemon[j];
+                                var pokedexInfo = pokeio.pokemonlist[parseInt(currentPokemon.PokedexTypeId)-1]; //TODO(b.jehanno): let's store in client
+                                var pokemon = {
+                                    encounterId: currentPokemon.EncounterId.toNumber(),
+                                    name: pokedexInfo.name,
+                                    lat: currentPokemon.Latitude,
+                                    lng: currentPokemon.Longitude,
+                                    expirationTimeMs: currentPokemon.ExpirationTimeMs.toNumber() != -1
+                                        ? currentPokemon.ExpirationTimeMs.toNumber() : 15*60*1000, //When expirationTime = -1 it means that it is between 15 and 30 min
+                                    image: pokedexInfo.img
+                                }
+                                pokemonsSeen.push(pokemon);
+                                console.log('[+] ' + pokedexInfo.name + ' found: ' + JSON.stringify(pokemon));
+                            }
+                        }
+                        if (pokemonsSeen.length > 0)
+                            io.sockets.emit('new_pokemons', pokemonsSeen);
+
+                        loggedUsers[username].lookingPointIndex = (loggedUsers[username].lookingPointIndex + 1) % (lookingPoints.length);
+                    });
+                }
             });
-        }, 10000);
+        }, 2000);
 
         //TODO(b.jehanno): First two chars are always /# that could have to do with rooms of socket.io
-        loggedUsers[username] = { pokeio: pokeio, socketId: '/#' + socketId, heartbeatId: heartbeatId };
+        loggedUsers[username] = {
+            pokeio: pokeio,
+            socketId: '/#' + socketId,
+            heartbeatId: heartbeatId,
+            location: { lat: pokeio.playerInfo.latitude, lng: pokeio.playerInfo.longitude },
+            lookingPointIndex: 0
+        };
     });
+}
+
+//Geometric stuff
+
+var pkmnRadius = 70; // radius from where you seen wild pokemons on the map
+
+var addDistance = function(latLng, dx, dy) {
+    var lat = latLng.lat + (180/Math.PI)*(dy/6378137)
+    var lng = latLng.lng + (180/Math.PI)*(dx/6378137) / Math.cos(Math.PI/180.0 * latLng.lat)
+    return { lat: lat, lng: lng };
+}
+
+var getSurroundingPoints = function(center) {
+    var points = [];
+    points.push(center);
+    points.push(addDistance(center, -pkmnRadius/2,  pkmnRadius));
+    points.push(addDistance(center,  pkmnRadius/2,  pkmnRadius));
+    points.push(addDistance(center,  pkmnRadius,    0         ));
+    points.push(addDistance(center,  pkmnRadius/2, -pkmnRadius));
+    points.push(addDistance(center, -pkmnRadius/2, -pkmnRadius));
+    points.push(addDistance(center, -pkmnRadius,    0         ));
+    return points;
 }
 
