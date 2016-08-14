0a6250f271e5d0ff577409151b01abdfc9f153b8
dekajoo <dekajoo@gmail.com>
2016-08-14 16:44:06 +0200

    Change default server port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 class Server {
 	static init() {
 		var app = express();
-		var port = process.env.PORT || 3000;
+		var port = process.env.PORT || 3100;
 
 		// Serve static files
 		app.use(express.static('public'));
